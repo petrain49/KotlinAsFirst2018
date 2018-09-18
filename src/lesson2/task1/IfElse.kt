a3a22e30c502e11238bf83a8370be46df454a48e
alekseivoroshilov <nekobeardetect@mail.ru>
2018-09-18 16:37:44 +0300

    asd

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -62,7 +62,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String = when {
+    age % 10 in 5..9 -> "$age лет"
+    age % 100 in 10..20 -> "$age лет"
+    age % 10 == 1 -> "$age год"
+    age % 10 in 2..4 -> "$age года"
+    else -> "$age лет"
+
+}
 
 /**
  * Простая
@@ -73,7 +80,18 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val hals = (v1 * t1 + v2 * t2 + v3 * t3) / 2 // половина пути
+    val s1 = v1 * t2
+    val s2 = v2 * t2
+
+    return when {
+        s1 >= hals -> hals / v1
+        s1 + s2 >= hals -> t1 + (hals - s1) / v2
+        else -> t1 + t2 + (hals - s1 - s2) / v3
+    }
+}
+
 
 /**
  * Простая
