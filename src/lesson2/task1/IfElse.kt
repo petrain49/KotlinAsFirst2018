545df608fddfd9486e64eeffd4805504c65567ee
Petr <4petrain9@gmail.com>
2018-09-19 23:28:02 +0300

    аа

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -104,7 +104,16 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    return when{
+        (kingX == rookX1 || kingY == rookY1) &&
+                (kingX == rookX2 || kingY == rookY2) -> 3
+        kingX == rookX2 || kingY == rookY2 -> 2
+        kingX == rookX1 || kingY == rookY1 -> 1
+        else -> 0
+
+    }
+}
 
 /**
  * Простая
